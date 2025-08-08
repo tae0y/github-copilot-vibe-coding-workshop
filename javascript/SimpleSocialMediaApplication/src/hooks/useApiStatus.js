34573966b936e31f2bb3456263ae04e0cf359fdd
박태영 <tae0yp@gmail.com>
2025-08-08 14:27:32 +0000

    Replace scafold

--- javascript/SimpleSocialMediaApplication/src/hooks/useApiStatus.js
+++
@@ -1,7 +0,0 @@
-import { useState } from 'react';
-
-export default function useApiStatus() {
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
-  return { error, setError, loading, setLoading };
-}
